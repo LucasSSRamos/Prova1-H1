0862b34db6aa22cf843440ba7b7ce7f49f06df11
humbertomelo <humbertomelo@UNI-LAB5-PROF>
2023-09-26 20:55:21 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/ClassController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WebApplication1.Models;
+using Newtonsoft.Json;
+
+
+namespace WebApplication1.Controllers
+
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        [HttpPost("ReceberCartas")]
+        public IActionResult Class([FromBody] ClassModel carta)
+        {
+            if (carta == null)
+            {
+                return BadRequest("Dados inválidos.");
+            }
+
+            if (string.IsNullOrEmpty(carta.Nome) || carta.Nome.Length < 3 || carta.Nome.Length > 255)
+            {
+                return BadRequest("Nome é obrigatório e deve ter entre 3 e 255 caracteres.");
+            }
+
+            // Ler cartas existentes
+            var cartas = LerCartas();
+
+            cartas.Add(carta);
+
+            // Salvar no arquivo JSON
+            SalvarCartas(cartas);
+
+            return Ok("Carta Enviada!");
+
+        }
+
+        [HttpGet("ListarCartas")]
+        public IActionResult Get()
+        {
+            var cartas = LerCartas();
+            return Ok(cartas);
+        }
+
+        private List<ClassModel> LerCartas()
+        {
+            if (System.IO.File.Exists(CartasFilePath))
+            {
+                var json = System.IO.File.ReadAllText(CartasFilePath);
+                return JsonConvert.DeserializeObject<List<ClassModel>>(json);
+            }
+            else
+            {
+                return new List<ClassModel>();
+            }
+        }
+
+        private void SalvarCartas(List<ClassModel> cartas)
+        {
+            var json = JsonConvert.SerializeObject(cartas);
+            System.IO.File.WriteAllText(CartasFilePath, json);
+        }
+    }
+}
---
+++ WebApplication1/Models/ClassModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace WebApplication1.Models
+{
+    public class ClassModel
+    {
+        public string Nome { get; set; }
+        public string rua { get; set; }
+        public int numero { get; set; }
+        public string cidade { get; set; }
+        public string bairro { get; set; }
+        public string estado { get; set; }
+        public int idade { get; set; }
+        public string texto_carta { get; set; }
+    }
+}
